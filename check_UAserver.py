868b7be917678a7bbff0c78890cea1ed7aaf129c
uklee <arn0287@mail.ru>
2021-05-20 11:37:23 +0900

    BUGFIX period doubling solved

--- check_UAserver.py
+++ check_UAserver.py
@@ -7,6 +7,7 @@ from win10toast import ToastNotifier
 
 
 CHECK_PERIOD = 300  # unit: sec
+# CHECK_PERIOD = 10  # fortest
 RELIABLE_URL = "https://1.1.1.1/"
 URL_TO_CHECK = ["https://student.kaist.ac.kr/web/main",
                 "https://student.kaist.ac.kr/web/api/banners",
@@ -18,7 +19,7 @@ URL_TO_CHECK = ["https://student.kaist.ac.kr/web/main",
 
 
 def check(URL): # False may caused by either Server problem or internet connection problem
-    # return False #fortest
+    # return False # fortest
     try:
         response = requests.get(URL)
         if (response.status_code == 200):
@@ -32,7 +33,7 @@ def check(URL): # False may caused by either Server problem or internet connecti
 
 
 def check_internet_connection():
-    # return True #fortest
+    # return True # fortest
     return check(RELIABLE_URL)
     
 
@@ -50,47 +51,61 @@ def resource_path(relative_path):   # to find icon file
 
 
 toaster = ToastNotifier()
+toaster.show_toast(title    = "check_UA SERVER",
+                   msg      = "Server check start successfully\n" + time.strftime('%c', time.localtime(time.time())),
+                   icon_path= resource_path("UA.ico"),
+                   duration = CHECK_PERIOD,
+                   threaded = True
+                   )
+
+
+
+
 
 while (True):
-        server_status = True
+    server_status = True
+    unavailable_URL = []
 
-        
-        for URL in URL_TO_CHECK:
-            if (not check(URL)):
-                server_status = False
-
-
-
-
-
-        if (server_status):                     # Server Fine
-            print("Server fine.", time.strftime('%c', time.localtime(time.time()))) # Not will be shown in final program. (hidden shell)
-
-        
-        else:
-            if (check_internet_connection()):   # Server in TROUBLE
-                print("Please check the server!!", time.strftime('%c', time.localtime(time.time())))
-                toaster.show_toast(title    = "UA SERVER WARNING",
-                                   msg      = "Cannot retrieve request from server properly. Please Check\n" + time.strftime('%c', time.localtime(time.time())),
-                                   icon_path= resource_path("UA.ico"),
-                                   duration = CHECK_PERIOD
-                                   )
-
-                
-            else:                               # Internet Problem
-                print("Internet Connection Problem..", time.strftime('%c', time.localtime(time.time())))
-
-                """
-                Or you may use below, if you want to alert your connection problem
-                toaster.show_toast(title    = "INTERNAL WARNING",
-                                   msg      = "Found internet connection problem.. Please Check\n" + time.strftime('%c', time.localtime(time.time())),
-                                   icon_path="UA.ico",
-                                   duration = CHECK_PERIOD
-                                   )
-                """
+    
+    for URL in URL_TO_CHECK:
+        if (not check(URL)):
+            server_status = False
+            unavailable_URL.append(URL)
 
 
 
 
 
-        time.sleep(CHECK_PERIOD)
+    if (server_status):                     # Server Fine
+        print("Server fine.", time.strftime('%c', time.localtime(time.time()))) # Not will be shown in final program. (hidden shell)
+
+    
+    else:
+        if (check_internet_connection()):   # Server in TROUBLE
+            print("Please check the server!!", time.strftime('%c', time.localtime(time.time())))
+            toaster.show_toast(title    = "UA SERVER WARNING",
+                               msg      = "Cannot retrieve request from server properly. Please Check\n" + str(unavailable_URL) + '\n' + time.strftime('%c', time.localtime(time.time())),
+                               icon_path= resource_path("UA.ico"),
+                               duration = CHECK_PERIOD,
+                               threaded = True
+                               )
+
+            
+        else:                               # Internet Problem
+            print("Internet Connection Problem..", time.strftime('%c', time.localtime(time.time())))
+
+            """
+            Or you may use below, if you want to alert your connection problem
+            toaster.show_toast(title    = "INTERNAL WARNING",
+                               msg      = "Found internet connection problem.. Please Check\n" + time.strftime('%c', time.localtime(time.time())),
+                               icon_path="UA.ico",
+                               duration = CHECK_PERIOD,
+                               threaded = True
+                               )
+            """
+
+
+
+
+
+    time.sleep(CHECK_PERIOD)
